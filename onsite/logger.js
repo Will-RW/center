96599561dd91e6490e07aa616c3478bd3f5bcb31
Will-RW <will@r-w.center>
2025-07-16 11:12:10 -0500

    Delete onsite/logger.js

--- onsite/logger.js
+++
@@ -1,6 +0,0 @@
-// onsite/logger.js
-module.exports = {
-  info:  (...args) => console.log('[INFO ]',  ...args),
-  warn:  (...args) => console.warn('[WARN ]', ...args),
-  error: (...args) => console.error('[ERROR]', ...args)
-};
