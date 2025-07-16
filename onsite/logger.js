ad495e5b9e6c8c474fb621b76dcd83c4990ca65f
Will-RW <will@r-w.center>
2025-07-16 11:04:03 -0500

    Create logger.js

---
+++ onsite/logger.js
@@ -0,0 +1,6 @@
+// onsite/logger.js
+module.exports = {
+  info:  (...args) => console.log('[INFO ]',  ...args),
+  warn:  (...args) => console.warn('[WARN ]', ...args),
+  error: (...args) => console.error('[ERROR]', ...args)
+};
